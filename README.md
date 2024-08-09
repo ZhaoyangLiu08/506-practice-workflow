53d42203a11c8bfdb58c0eea64b9c1185c971278
ZhaoyangLiu08 <zhaoy.liu08@gmail.com>
2024-08-09 16:13:04 +0000

    this is a comment line

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # 506-practice-workflow
-this assignment is as a primer for 504/507 Fall semester classes to practice a basic development workflow
+This assignment is as a primer for 504/507 Fall semester classes to practice a basic development workflow
+
+These variables are related to the course year, assignment file name, random list, and the introduction of the student (me).
+
+This function is used to calculate the final grade and letter grade, the final grade is based on grade 1 and grade 2. If your grade >= 90, you can get an "A", otherwise you can only get a "B".
+In my case, I got 92 on grade1 and 85 on grade2. So my final grade = 92*0.6+85*0.4 = 89.1999 < 90 ,so my letter grade is 'B'.
--- intro_practice.py
+++ intro_practice.py
@@ -1,1 +1,40 @@
 #this is a comment line
+
+#importing packages
+import pandas as pd
+import numpy as np
+
+#creatuing part
+#creating variables
+year = 2024 # a number variable
+file_name = "HHA-506-practice" # a string variable
+list_variable = [1,2,3,4,5] # a list
+student_intro = {
+    'name': 'zhaoyangliu',
+    'age' : 27,
+    'schools' : ['SBU', 'BJFU'],
+    'adress' : {'city':'flushing' , 'state':'NY'}
+} #a dictionary with a list and a nested dictionary
+
+#creating a function
+def function_final_grade(grade1,grade2):
+    final_grade = 0.6 * grade1 + 0.4 * grade2
+    if final_grade >=90:
+        letter_grade = 'A'
+    else:
+         letter_grade = 'B'
+
+    return final_grade, letter_grade
+
+#printing part
+print("number variable", year)
+print("string variable", file_name)
+print("list variable", list_variable)
+print("dictionary variable", student_intro)
+
+#function example
+grade1 = 92
+grade2 = 85
+final_grade, letter_grade = function_final_grade(grade1,grade2)
+print("Final Grade", final_grade)
+print("Letter Grade", letter_grade)
